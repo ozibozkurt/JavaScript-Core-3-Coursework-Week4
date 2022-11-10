78f77270458f7fbf2a42ea516f7eb4b7ab7d9b0e
ozibozkurt <ozipaki@gmail.com>
2022-11-10 13:18:04 +0000

    car

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,18 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  let salesObject = {};
 
+  /*CREATE*/
+  carsSold.forEach((car) => {
+    if (car.make !== salesObject[car.make]) {
+      salesObject[car.make] = 0;
+    }
+  });
+
+  /*UPDATE*/
+  carsSold.forEach((car) => {
+    salesObject[car.make] += car.price;
+  });
+
+  return salesObject;
+}
 module.exports = sales;
